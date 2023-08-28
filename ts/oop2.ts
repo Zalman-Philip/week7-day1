cd5c22b3476bb23683cc0024a9ad120cebdfcb28
Zalman-Philip <z0533147971@gmail.com>
2023-08-28 09:03:10 +0300

    second commit

--- ts/oop2.ts
+++ ts/oop2.ts
@@ -72,8 +72,8 @@ class MedicalStaff extends Person {
 class Doctor extends MedicalStaff {
   specialization: string;
   #doctorID: string;
-  availability: {[key: string]: {[key: string]: unknown}};
-  range: number;
+  availability: { [key: string]: { [key: string]: boolean } };
+  range: { min: number; max: number };
   constructor(
     firstName: string,
     lastName: string,
@@ -81,11 +81,11 @@ class Doctor extends MedicalStaff {
     doctorID: string,
     age: number,
     address: string,
-    availability: {[key: string]: {[key: string]: unknown}},
+    availability: { [key: string]: { [key: string]: boolean } },
     staffID: string,
     position: string,
     department: string,
-    range: number
+    range: { min: number; max: number }
   ) {
     super(staffID, position, department, firstName, lastName, age, address);
     this.specialization = specialization;
@@ -177,10 +177,6 @@ class Hospital {
     this.doctors.push(doctor);
     return this;
   }
-
-  addAppointment(appointment: Appointment) {
-    this.appointments.push(appointment);
-  }
   logAllAppointments() {
     this.appointments.forEach((appointment) =>
       console.log(appointment.AppointmentInfo())
@@ -223,47 +219,192 @@ class Hospital {
       (record) => record.patient.getID() === patientID
     );
   }
-//   getDoctorSchedule(doctorID: string, date: string) {
-//     return this.appointments.filter((appointment) => {
-//      return (appointment.doctor.getID() === doctorID &&
-//         appointment.status === "scheduled" &&
-//         appointment.date === date);
-//     });
-//   }
-// getDoctorSchedule(doctorID: string, date: string) {
-//    const doctor = this.doctors.find((doctor) => doctor.getID() === doctorID)
-//    doctor.availability.date === date
-// }
-//   getDoctorAvailability(doctorID: string, date: string) {
-//     return this.appointments.filter((appointment) => {
-//         return(appointment.doctor.getID() === doctorID &&
-//         appointment.status !== "scheduled" && 
-//         appointment.date === date);
-        
-//   })
-// }
-// }
-const patient1 = new Patient("avi", "israely", "bvfyuf3");
-const patient2 = new Patient("yheuda", "cohen", "bvfy>3");
-const patient3 = new Patient("shlomo", "poleg", "mnbjkig$%2");
+  getDoctorSchedule(doctorID: string, date: string) {
+    const doctor = this.doctors.find((doctor) => doctor.getID() === doctorID);
+    if (!doctor) return;
+    const day = doctor.availability[date];
+    const arr = Object.keys(day);
+    const available = arr.filter((key) => day[key] === false);
+    return available;
+  }
+  getDoctorAvailability(doctorID: string, date: string) {
+    const doctor = this.doctors.find((doctor) => doctor.getID() === doctorID);
+    if (!doctor) return;
+    const day = doctor.availability[date];
+    const arr = Object.keys(day);
+    const available = arr.filter((key) => day[key] === true);
+    return available;
+  }
+  addAppointment(appointment: Appointment) {
+    const { date, time } = appointment;
+    const doctorID = appointment.doctor.getID();
+    const doctor = this.doctors.find((doctor) => doctor.getID() === doctorID);
+    if (!doctor) return;
+    const day = doctor.availability[date];
+    if(!day) return
+    const arr = Object.keys(day);
+    const available = arr.find((key) => key === time && day[key] === true);
+    if (!available) return;
+    if (
+      appointment.patient.age > doctor.range.max ||
+      appointment.patient.age < doctor.range.min
+    )
+      return;
+    this.appointments.push(appointment);
+    arr.map((key) => {
+      if (key === time) day[key] = false;
+      return this
+    });
+  }
+}
+
+const doctor1 = new Doctor(
+  "David",
+  "Salomon",
+  "Surgeon",
+  "dfg%%%^",
+  35,
+  "123 Main St",
+  {
+    "08/30/2023": {
+      "09:00": true,
+      "10:00": false,
+      "11:00": true,
+    },
+    "08/31/2023": {
+      "09:00": true,
+      "10:00": true,
+      "11:00": true,
+    },
+  },
+  "staff123",
+  "Senior Surgeon",
+  "Surgery Department",
+  { min: 15, max: 30 }
+);
+
+const doctor2 = new Doctor(
+  "Sarah",
+  "Johnson",
+  "Pediatrician",
+  "abc123",
+  40,
+  "456 Elm St",
+  {
+    "08/30/2023": {
+      "09:00": true,
+      "10:00": true,
+      "11:00": false,
+    },
+    "08/31/2023": {
+      "09:00": false,
+      "10:00": true,
+      "11:00": true,
+    },
+  },
+  "staff456",
+  "Pediatric Department",
+  "Pediatrics",
+  { min: 20, max: 35 }
+);
+
+const doctor3 = new Doctor(
+  "Michael",
+  "Smith",
+  "Cardiologist",
+  "xyz789",
+  45,
+  "789 Oak St",
+  {
+    "08/30/2023": {
+      "09:00": false,
+      "10:00": true,
+      "11:00": true,
+    },
+    "08/31/2023": {
+      "09:00": true,
+      "10:00": true,
+      "11:00": true,
+    },
+  },
+  "staff789",
+  "Cardiology Department",
+  "Cardiology",
+  { min: 25, max: 40 }
+);
+const patient1 = new Patient(
+  "Alice",
+  "Johnson",
+  "p12345",
+  28,
+  "123 Elm St",
+  "555-1234",
+  "555-5678",
+  []
+);
+
+const patient2 = new Patient(
+  "Bob",
+  "Smith",
+  "p67890",
+  35,
+  "456 Oak St",
+  "555-5678",
+  "555-9012",
+  []
+);
+
+const patient3 = new Patient(
+  "Eva",
+  "Williams",
+  "p23456",
+  34,
+  "789 Maple St",
+  "555-7890",
+  "555-1234",
+  []
+);
+
 const patients = [patient1, patient2, patient3];
-
-const doctor1 = new Doctor("david", "salomon", "surgeon", "dfg%%%^");
-const doctor2 = new Doctor("moshe", "ben-amy", "plastic surgeon", "VFw7&&sx");
-const doctor3 = new Doctor("yakov", "refaelov", "Neurosurgeon", "v7^&fd");
 const doctors = [doctor1, doctor2, doctor3];
-
-const appointment1 = new Appointment(patient1, doctor1, "23/05/2023", "14:30");
-const appointment2 = new Appointment(patient2, doctor2, "23/05/2023", "09:34");
-const appointment3 = new Appointment(patient3, doctor3, "07/08/2023", "17:55");
+const appointment1 = new Appointment(
+  patient1,
+  doctor1,
+  "23/05/2023",
+  "14:30",
+  "scheduled"
+);
+const appointment2 = new Appointment(
+  patient2,
+  doctor2,
+  "23/05/2023",
+  "09:34",
+  "cancelled"
+);
+const appointment3 = new Appointment(
+  patient3,
+  doctor3,
+  "07/08/2023",
+  "17:55",
+  "scheduled"
+);
 const appointments = [appointment1, appointment2, appointment3];
 
-const hospital = new Hospital("Hospital1", doctors, patients, appointments);
-const appointment4 = new Appointment(patient3, doctor2, "09/08/2023", "13:35");
-hospital.addAppointment(appointment4);
+const hospital = new Hospital("Hospital1", doctors, patients, appointments, []);
+const appointment4 = new Appointment(
+  patient3,
+  doctor2,
+  "08/30/2023",
+  "11:00",
+  "scheduled"
+);
+
+const result = hospital.addAppointment(appointment4);
 
 hospital.logAllAppointments();
 hospital.logAppointmentsByDate("23/05/2023");
-hospital.logAppointmentsByDoctorID("VFw7&&sx");
-hospital.logAppointmentsByPatientID("bvfy>3");
-const availability = []
+hospital.logAppointmentsByDoctorID("dfg%%%^");
+hospital.logAppointmentsByPatientID("p67890");
+console.log("in app");
+console.log(result);
+
